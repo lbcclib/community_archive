b016a4c8140b599b50bb4aff2a131581e9ed4de4
Jane Sandberg <sandbej@linnbenton.edu>
2017-09-21 17:02:36 -0700

    Revising the newspaper issue form

--- app/forms/hyrax/newspaper_issue_form.rb
+++ app/forms/hyrax/newspaper_issue_form.rb
@@ -3,7 +3,9 @@
 module Hyrax
   class NewspaperIssueForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperIssue
-    self.terms += [:resource_type]
-    self.terms -= [:source, :publisher]
+    self.required_fields += [:contributor_list, :date_created]
+    self.required_fields -= [:creator, :rights_statement]
+    self.terms += [:contributor_list]
+    self.terms -= [:abstract, :creator, :contributor, :description, :identifier, :source, :subject, :publisher, :related_url]
   end
 end
--- app/models/newspaper_issue.rb
+++ app/models/newspaper_issue.rb
@@ -2,6 +2,7 @@
 #  `rails generate hyrax:work NewspaperIssue`
 class NewspaperIssue < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
+  #include Hydra::Derivatives
 
   self.indexer = NewspaperIssueIndexer
   # Change this to restrict which works can be added as a child.
@@ -9,6 +10,11 @@ class NewspaperIssue < ActiveFedora::Base
   validates :title, presence: { message: 'Your work must have a title.' }
 
   self.human_readable_type = 'Newspaper Issue'
+
+  property :contributor_list, predicate: ::RDF::URI('http://purl.org/dc/terms/contributor'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
 
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
