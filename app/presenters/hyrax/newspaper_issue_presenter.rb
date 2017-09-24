b51b58de8392e716b3012c89e90c583ab993d06f
Jane Sandberg <sandbej@linnbenton.edu>
2017-09-24 10:59:52 -0700

    adding a presenter for newspaper issues

--- app/presenters/hyrax/newspaper_issue_presenter.rb
+++ app/presenters/hyrax/newspaper_issue_presenter.rb
@@ -2,5 +2,6 @@
 #  `rails generate hyrax:work NewspaperIssue`
 module Hyrax
   class NewspaperIssuePresenter < Hyrax::WorkShowPresenter
+    delegate :edition, :volume, :contributor_list, to: :solr_document
   end
 end
