1ade08342ad42f743415c3840089429680480ace
Rob Kaufman <rob@notch8.com>
2017-10-03 18:52:30 -0700

    fix rake

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -1,27 +1,11 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+server "libarchive.linnbenton.edu", user: "notch8", roles: %w{app db web}
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+set :systemd_unit, "hyrax-beta.service"
+set :systemd_use_sudo, true
+set :systemd_roles, %w(app)
 
 
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
+set :deploy_to, "/var/www/hyrax-beta"
 
 # Configuration
 # =============
