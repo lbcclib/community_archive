e503c49505e9470e1b4702deae025848cbeff6a9
Jane Sandberg <sandbej@linnbenton.edu>
2017-11-20 12:45:29 -0800

    Enabling sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
+
+redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/" }
+
+Sidekiq.configure_server do |s|
+	s.redis = redis_conn
+end
+
