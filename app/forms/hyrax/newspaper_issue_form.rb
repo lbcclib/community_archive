9bcad060f49ea027b5fd327490f2a3e49031f800
Jane Sandberg <sandbej@linnbenton.edu>
2017-11-28 12:05:23 -0800

    Contributor list is no longer required field

--- app/forms/hyrax/newspaper_issue_form.rb
+++ app/forms/hyrax/newspaper_issue_form.rb
@@ -3,7 +3,7 @@
 module Hyrax
   class NewspaperIssueForm < Hyrax::Forms::WorkForm
     self.model_class = ::NewspaperIssue
-    self.required_fields += [:contributor_list, :date_created, :volume, :edition]
+    self.required_fields += [:date_created, :volume, :edition]
     self.required_fields -= [:creator, :rights_statement]
     self.terms += [:contributor_list, :volume, :edition]
     self.terms -= [:abstract, :creator, :contributor, :description, :identifier, :license, :source, :subject, :publisher, :related_url]
