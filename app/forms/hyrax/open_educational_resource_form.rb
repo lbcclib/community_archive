5100d04bade70293e5098535557314d4a812ae89
Michaela Willi Hooper <mdh@umich.edu>
2017-12-06 14:26:56 -0800

    Moved description to required and removed resource_type
    to harmonize with OERC metadata

--- app/forms/hyrax/open_educational_resource_form.rb
+++ app/forms/hyrax/open_educational_resource_form.rb
@@ -3,8 +3,8 @@
 module Hyrax
   class OpenEducationalResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::OpenEducationalResource
-    self.terms += [:resource_type, :crn, :year_course_taught]
-    self.required_fields += [:license]
+    self.terms += [:crn, :year_course_taught]
+    self.required_fields += [:license, :description]
     self.terms -= [:source]
     self.required_fields -= [:rights_statement]
   end
