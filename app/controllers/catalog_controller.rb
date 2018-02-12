c605588df2ac4a581ced4b6cebac6c6d3dd4c0dc
Jane Sandberg <sandbergja@users.noreply.github.com>
2018-02-12 14:54:33 -0800

    adding CRN to show view

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -103,6 +103,7 @@ class CatalogController < ApplicationController
     config.add_show_field solr_name("resource_type", :stored_searchable), label: "Resource Type"
     config.add_show_field solr_name("format", :stored_searchable)
     config.add_show_field solr_name("identifier", :stored_searchable)
+    config.add_show_field solr_name("crn", :stored_searchable)
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
