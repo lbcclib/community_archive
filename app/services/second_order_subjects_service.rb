0168d2bd648de6db75a4e79d1207c9d0244c9bc8
Michaela Willi Hooper <willihm@linnbenton.edu>
2018-02-23 10:43:59 -0800

    Created second order subject service with updated file name

---
+++ app/services/second_order_subjects_service.rb
@@ -0,0 +1,15 @@
+# Service to add the OERC second order subjects controlled vocabulary
+module SecondOrderSubjectService
+  mattr_accessor :authority
+  self.authority = Qa::Authorities::Local.subauthority_for('second_order_subjects')
+
+  def self.select_all_options
+    authority.all.map do |element|
+      [element[:label], element[:id]]
+    end
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term')
+  end
+end
