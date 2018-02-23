f73d90aed3c8814dfff12eba69795259d0fcf7cf
Michaela Willi Hooper <willihm@linnbenton.edu>
2018-02-23 11:02:12 -0800

    making subject required

--- app/forms/hyrax/open_educational_resource_form.rb
+++ app/forms/hyrax/open_educational_resource_form.rb
@@ -4,7 +4,7 @@ module Hyrax
   class OpenEducationalResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::OpenEducationalResource
     self.terms += [:crn, :year_course_taught]
-    self.required_fields += [:license, :description]
+    self.required_fields += [:license, :description, :subject]
     self.terms -= [:source]
     self.required_fields -= [:rights_statement]
   end
