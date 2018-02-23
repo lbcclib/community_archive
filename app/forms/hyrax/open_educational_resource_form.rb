b743e0300226b6bc2fc039c6578c0b70412cab41
Jane Sandberg <sandbej@linnbenton.edu>
2018-02-23 13:01:15 -0800

    OERC: fixing field names on form

--- app/forms/hyrax/open_educational_resource_form.rb
+++ app/forms/hyrax/open_educational_resource_form.rb
@@ -3,8 +3,8 @@
 module Hyrax
   class OpenEducationalResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::OpenEducationalResource
-    self.terms += [:crn, :year_course_taught, :media_type, :material_format]
-    self.required_fields += [:license, :description, :subject, :media_type, :material_format]
+    self.terms += [:crn, :year_course_taught, :media_format, :material_type]
+    self.required_fields += [:license, :description, :subject, :media_format, :material_type]
     self.terms -= [:source]
     self.required_fields -= [:rights_statement]
   end
