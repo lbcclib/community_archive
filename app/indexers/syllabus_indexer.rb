b94ecd10775e7f4871da3f1604db910ccbe507f0
Jane Sandberg <sandbej@linnbenton.edu>
2018-03-02 05:19:30 -0800

    better indexing of courses like PE185A

--- app/indexers/syllabus_indexer.rb
+++ app/indexers/syllabus_indexer.rb
@@ -13,10 +13,13 @@ class SyllabusIndexer < Hyrax::WorkIndexer
   def generate_solr_document
   # Making sure that course numbers are indexed both with and without spaces
     super.tap do |solr_doc|
-      matches = /^([A-Z]{2,4})\s?([0-9]{3}.*)$/.match(object.title.first.upcase)
+      matches = /^([A-Z]{2,4})\s?([0-9]{3})(.*)$/.match(object.title.first.upcase)
       if matches and (3 == matches.size)
         solr_doc['title_tesim'] = matches[1] + ' ' + matches[2]
         solr_doc['course_prefix_number_concat_tesim'] = matches[1] + matches[2]
+      elsif matches and (4 == matches.size)
+        solr_doc['title_tesim'] = matches[1] + ' ' + matches[2] + matches[3]
+        solr_doc['course_prefix_number_concat_tesim'] = matches[1] + matches[2] + matches[3] + ' ' + matches[1] + matches[2] + ' ' + matches[2] + ' ' + matches[3]
       end
     end
   end
