27576f74b4c54bcabcc871c5ced27b1de6cbd726
Jane Sandberg <sandbej@linnbenton.edu>
2018-03-02 05:55:01 -0800

    fixing normalization for crosslisted courses

--- app/indexers/syllabus_indexer.rb
+++ app/indexers/syllabus_indexer.rb
@@ -13,13 +13,22 @@ class SyllabusIndexer < Hyrax::WorkIndexer
   def generate_solr_document
   # Making sure that course numbers are indexed both with and without spaces
     super.tap do |solr_doc|
-      matches = /^([A-Z]{2,4})\s?([0-9]{3,4})([A-Z])$/.match(object.title.first.upcase)
-      if matches and (3 == matches.size)
-        solr_doc['title_tesim'] = matches[1] + ' ' + matches[2]
-        solr_doc['course_prefix_number_concat_tesim'] = matches[1] + matches[2]
-      elsif matches and (4 == matches.size)
-        solr_doc['title_tesim'] = matches[1] + ' ' + matches[2] + matches[3]
-        solr_doc['course_prefix_number_concat_tesim'] = matches[1] + matches[2] + matches[3] + ' ' + matches[1] + matches[2] + ' ' + matches[2] + ' ' + matches[3]
+      solr_doc['title_tesim'] = []
+      solr_doc['course_prefix_number_concat_tesim'] = []
+      object.title.each do |title| 
+        matches = /^([A-Z]{2,4})\s?([0-9]{3,4})([A-Z]?)$/.match(title.upcase)
+        if matches and (4 == matches.size)
+          if ('' == matches[3])
+	    puts solr_doc['title_tesim'].class
+            solr_doc['title_tesim'] << matches[1] + ' ' + matches[2]
+            solr_doc['course_prefix_number_concat_tesim'] << matches[1] + matches[2]
+	  else
+            solr_doc['title_tesim'] << matches[1] + ' ' + matches[2] + matches[3]
+            solr_doc['course_prefix_number_concat_tesim'] << matches[1] + matches[2] + matches[3] + ' ' + matches[1] + matches[2] + ' ' + matches[2] + ' ' + matches[3]
+          end
+	else
+          solr_doc['title_tesim'] << title.upcase
+        end
       end
     end
   end
