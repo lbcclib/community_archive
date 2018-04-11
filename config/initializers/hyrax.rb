cd28b4620e8f001d047c4a0c3aed4709e81d6c7c
Jane Sandberg <sandbej@linnbenton.edu>
2018-04-10 20:14:30 -0700

    Adding an Image work type

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -7,6 +7,8 @@ Hyrax.config do |config|
   config.register_curation_concern :document
   # Injected via `rails g hyrax:work OpenEducationalResource`
   config.register_curation_concern :open_educational_resource
+  # Injected via `rails g hyrax:work Image`
+  config.register_curation_concern :image
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
