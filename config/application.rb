4c09889fc2687d410bf54dbcce5c6a44b4202ec7
Jane Sandberg <sandbej@linnbenton.edu>
2020-11-12 20:12:22 -0800

    Use different active job adapters for different environments, closes #32

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Hyrax
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
