b155729b5405ef76de265200a4e9fedd5ce7dba4
Jane Sandberg <sandbej@linnbenton.edu>
2021-10-16 01:00:58 -0700

    Don't seed db on initial helm install

--- .helm/deploy.rb
+++ .helm/deploy.rb
@@ -12,15 +12,8 @@ class DeployToKubernetes < Thor
        'install a new helm release (such as a new staging or testing environment)'
   def install(commarch_version, release_name = 'staging', hyrax_helm_chart_version = DEFAULT_HYRAX_HELM_CHART_VERSION)
     pull_and_update_charts hyrax_helm_chart_version
-    postgresql_password = new_password
     secrets_from_user = ask_user_for_secrets ['smtp.address', 'smtp.port', 'smtp.user', 'smtp.pass']
-    run_helm_command 'install', '--namespace=communityarchive', "--values=#{__dir__}/values.yml",
-                     "--set=image.tag=#{commarch_version}",
-                     "--set=worker.image.tag=#{commarch_version}",
-                     "--set=postgresql.postgresqlPassword=#{postgresql_password}",
-                     "--set=global.postgresql.postgresqlPassword=#{postgresql_password}",
-                     "--set=redis.password=#{new_password}",
-                     "--set=solr.authentication.adminPassword=#{new_password}",
+    run_helm_command 'install', *basic_install_flags(release_name, commarch_version),
                      *secrets_to_set_commands(secrets_from_user),
                      release_name, 'hyrax'
   end
@@ -29,11 +22,7 @@ class DeployToKubernetes < Thor
        'upgrade the given release (defaults to staging) to the requested version (commit hash)'
   def upgrade(commarch_version, release_name = 'staging', hyrax_helm_chart_version = DEFAULT_HYRAX_HELM_CHART_VERSION)
     pull_and_update_charts hyrax_helm_chart_version
-    run_helm_command 'upgrade', '--namespace=communityarchive', '--atomic',
-                     '--reuse-values',
-                     "--values=#{__dir__}/values.yml",
-                     "--set=image.tag=#{commarch_version}",
-                     "--set=worker.image.tag=#{commarch_version}",
+    run_helm_command 'upgrade', *basic_upgrade_flags(release_name, commarch_version),
                      release_name, 'hyrax'
   end
 
@@ -62,6 +51,32 @@ class DeployToKubernetes < Thor
     end
   end
 
+  def basic_install_flags(release_name, commarch_version)
+    postgresql_password = new_password
+    flags = basic_flags_for_install_and_upgrade(release_name, commarch_version)
+    flags << "--set=postgresql.postgresqlPassword=#{postgresql_password}"
+    flags << "--set=global.postgresql.postgresqlPassword=#{postgresql_password}"
+    flags << "--set=redis.password=#{new_password}"
+    flags << "--set=solr.authentication.adminPassword=#{new_password}"
+    flags << '--set=skipDbMigrateSeed=true' # Don't install DB, so that it can be created with pg_restore instead
+  end
+
+  def basic_upgrade_flags(release_name, commarch_version)
+    flags = basic_flags_for_install_and_upgrade(release_name, commarch_version)
+    flags << '--atomic'
+    flags << '--reuse-values'
+    flags << '--set=skipDbMigrateSeed=false' # Run db migrations on upgrade
+  end
+
+  def basic_flags_for_install_and_upgrade(release_name, commarch_version)
+    [
+      '--namespace=communityarchive',
+      values_files(release_name),
+      "--set=image.tag=#{commarch_version}",
+      "--set=worker.image.tag=#{commarch_version}"
+    ]
+  end
+
   def new_password
     SecureRandom.hex(42)
   end
@@ -76,6 +91,16 @@ class DeployToKubernetes < Thor
       "--set=\"extraEnvVars[#{index}].name=#{secret[:name]}\" "\
       "--set=\"extraEnvVars[#{index}].value=#{secret[:value]}\""
     end
+  end
+
+  def values_files(release_name)
+    options = "--values=#{values_file_path} "
+    options << "--values=#{values_file_path(release_name)}" if File.exist? values_file_path(release_name)
+    options
+  end
+
+  def values_file_path(name = 'values')
+    "#{__dir__}/#{name}.yml"
   end
 end
 
