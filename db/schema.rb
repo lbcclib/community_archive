b184d84672ce58897415ea2e3a50043001fdfc97
leaannbradford <ltrammer@gmail.com>
2022-08-24 13:02:14 -0700

    remove cas sso, add saml sso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_065737) do
+ActiveRecord::Schema.define(version: 2022_08_24_191233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -628,6 +628,8 @@ ActiveRecord::Schema.define(version: 2021_08_06_065737) do
     t.boolean "admin", default: false
     t.string "preferred_locale"
     t.string "username"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
